6d5ab80be3bb6cffef2970c3b70fba8091ca3731
Luke <reboluketion42@gmail.com>
2021-11-10 19:51:18 -0800

    navbar up and running w/ components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,56 +1,52 @@
-import React, {useEffect, useState} from "react";
-import { fetchMyRoutines } from "../api";
+import React from "react";
 import { Link } from "react-router-dom";
-import { clearCurrentUser, clearUserName, getToken} from "../auth";
+// import { clearCurrentUser, clearUserName, getToken} from "../auth";
 
 
 export default function Navbar(props) {
-    const { isLoggedIn, setIsLoggedIn } = props
+    // const { isLoggedIn, setIsLoggedIn } = props
 
-    const [personalData, setPersonalData] = useState([])
+    // const [personalData, setPersonalData] = useState([])
 
-    useEffect(async () => {
-      const TOKEN = getToken();
-      if (TOKEN) {
-        setIsLoggedIn(true);
-      }
-      const routines = await fetchMyRoutines();
-      setPersonalData(routines);
-    }, []);
+    // useEffect(async () => {
+     
+      
+      
+    // }, []);
 
   return (
     <nav className="navbar">
       <div>
         <Link className="links" to="/">Home</Link>
-        <Link className="links" to="/routines">Routines</Link>
-        <Link className="links" to="/activities">Activities</Link>
-        {isLoggedIn  ? 
-          <Link className="links" to="/myroutines">
-            MyRoutines
+        <Link className="links" to="/products">Products</Link>
+        <Link className="links" to="/cart">Cart</Link>
+        {/* {isLoggedIn  ?  */}
+          <Link className="links" to="/mybbq">
+            Account
           </Link>
-         : null}
-        {isLoggedIn ? (
-          <Link
-            className="links"
-            to="/login"
-            onClick={(event) => {
-              const course = confirm("Are you sure you want to log out?");
+        {/* //  : null}
+        // {isLoggedIn ? ( */}
+        {/* //   <Link */}
+        {/* //     className="links"
+        //     to="/login"
+        //     onClick={(event) => { */}
+        {/* //       // const course = confirm("Are you sure you want to log out?");
 
-              if (course === true) {
-                event.preventDefault;
-                clearCurrentUser();
-                clearUserName();
-                setIsLoggedIn(false);
-              }
-            }}
-          >
-            {isLoggedIn ? "LogOut" : "LogIn/SignUp"}
-          </Link>
-        ) : (
+        //       // if (course === true) { */}
+        {/* //       //   event.preventDefault;
+        //       //   clearCurrentUser();
+        //       //   clearUserName();
+        //       //   setIsLoggedIn(false);
+        //       // }
+        //     }}
+        //   > */}
+        {/* //     {isLoggedIn ? "LogOut" : "LogIn/SignUp"}
+        //   </Link>
+        // ) : ( */}
           <Link className="links" to="/login">
             Login/SignUp
           </Link>
-        )}
+        {/* // )} */}
       </div>
     </nav>
   );
