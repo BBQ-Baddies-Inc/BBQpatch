2b7cec0b2b0ea30338d2ceae514d643cc96f5987
Eric Flores <ericfullstack@gmail.com>
2021-11-10 21:36:08 -0800

    created users in DB and updated client and init_db

--- db/init_db.js
+++ db/init_db.js
@@ -1,31 +1,9 @@
-// code to build and initialize DB goes here
-const {
-  client
-  // other db methods 
-} = require('./index');
+/* 
+DO NOT CHANGE THIS FILE
+*/
+const { client } = require("./client");
+const { rebuildDB } = require("./seedData");
 
-async function buildTables() {
-  try {
-    client.connect();
-
-    // drop tables in correct order
-
-    // build tables in correct order
-
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function populateInitialData() {
-  try {
-    // create useful starting data
-  } catch (error) {
-    throw error;
-  }
-}
-
-buildTables()
-  .then(populateInitialData)
+rebuildDB()
   .catch(console.error)
   .finally(() => client.end());
