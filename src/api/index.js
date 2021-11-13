6ce2208c6fa2f2729782b3ffbafdac0feed4c501
Eric Flores <ericfullstack@gmail.com>
2021-11-13 12:26:05 -0800

    fix userRoutes

--- src/api/index.js
+++ src/api/index.js
@@ -15,55 +15,6 @@ export async function getSomething() {
 // export async function Users()
 // export async function EditUser()
 // export async function fetchProfile()
-const userRouter = require('../src/api/users')
-const express = require("express");
-const apiRouter = express.Router();
-const jwt = require("jsonwebtoken");
-const {JWT_SECRET = "neverTell"} = process.env
-const userRouter = require("./users");
-
-const {
-  getUserById,
-} = require("../db")
-
-apiRouter.get('/health', async (req, res)=>{
-  try{
-    res.send({message:"connected!"})
-  }catch(error){
-      console.error(error);
-      next(error)
-  }
-});
-
-apiRouter.use(async (req, res, next) => {
-  const prefix = 'Bearer ';
-  const auth = req.header('Authorization');
-  
-  if (!auth) { 
-    next();
-  } else if (auth.startsWith(prefix)) {
-    const token = auth.slice(prefix.length);
-    
-    try {
-      const parsedToken = jwt.verify(token, JWT_SECRET);
-      
-      const id = parsedToken && parsedToken.id
-      if (id) {
-        req.user = await getUserById(id);
-        next();
-      }
-    } catch (error) {
-      next(error);
-    }
-  } else {
-    next({
-      name: 'AuthorizationHeaderError',
-      message: `Authorization token must start with ${ prefix }`
-    });
-  }
-});
-
-apiRouter.use('./users', userRouter)
 
 
 // Products
@@ -85,4 +36,3 @@ apiRouter.use('./users', userRouter)
 // export async function payment();
 // export async function confirmPurchase()
 
-module.exports = apiRouter
--- src/api/users.js
+++ src/api/users.js
@@ -1,77 +1,4 @@
-const express = require("express");
-const userRouter = express.Router();
 
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET = "neverTell" } = process.env;
-
-const {
-  createUser,
-  getUserByUsername,
-  getUser,
-  getUserById,
-  
-} = require("../db");
-const requireUser = require("./utilities");
-
-userRouter.post("/register", async (req, res, next) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await getUserByUsername(username);
-
-    if (user) {
-      next({
-        name: "UserExistsError",
-        message: "Username already exists",
-      });
-    } else if (password.length < 8) {
-      next({
-        name: "PasswordTooShortError",
-        message: "Password must contain at least 8 characters",
-      });
-    } else {
-      const user = await createUser({ username, password });
-      const token = jwt.sign(
-        { id: user.id, username: user.username },
-        JWT_SECRET,
-        { expiresIn: "1w" }
-      );
-      res.send({ user, token, message: "You're logged in" });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-userRouter.post("/login", async (req, res, next) => {
-  const { username, password } = req.body;
-
-  try {
-    if (!username || !password) {
-      next({
-        message: "Incorrect username or password",
-      });
-    } else {
-      const user = await getUser({ username, password });
-      const token = jwt.sign(
-        { id: user.id, username: user.username },
-        JWT_SECRET,
-        { expiresIn: "1w" }
-      );
-      res.send({ user, token, message: "You're logged in" });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-userRouter.get("/me", requireUser, async (req, res, next) => {
-  try {
-    res.send(req.user);
-  } catch (error) {
-    next(error);
-  }
-});
 
 // userRouter.get("/:username/routines", async (req, res, next) => {
 //   const { username } = req.params;
@@ -84,4 +11,3 @@ userRouter.get("/me", requireUser, async (req, res, next) => {
 //   }
 // });
 
-module.exports = userRouter
