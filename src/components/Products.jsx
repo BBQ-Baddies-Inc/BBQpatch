30c617b8c7419c6d1b4cdd4ec1c77d3282c8136c
Brian Leay <brian@brianleay.com>
2021-11-20 14:00:30 -0800

    getting the react to work on the products page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 import { getProducts } from "../api/products";
 
 export default function Products() {
@@ -14,21 +17,40 @@ export default function Products() {
   //   console.log(allProducts)
   console.log(products);
   return (
-    <div>
+    <div className="products_page">
       <h1>Products</h1>
-      <ul>
+      <div className="card_positions">
         {products.map((product) => {
-          const {name, price, description, photos } = product;
+          const { name, price, description, photos } = product;
           return (
-            <li key={product.id}>
-              <h2>{name}</h2>
-              <p>{price}</p>
-              <p>{description}</p>
-              <a href={photos}></a>
-            </li>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>{description}</Card.Text>
+                <Button variant="primary">Add to Cart</Button>
+                <form>
+                  <input
+                    type="number"
+                    // placeholder=“quantity-of-products”
+                    min="1"
+                    max="10"
+                  ></input>
+                  <Link to={`/cart`}>
+                    <button>Add To Cart</button>
+                  </Link>
+                </form>
+              </Card.Body>
+            </Card>
+            // <div className="card" key={product.id}>
+            //   <h2>{name}</h2>
+            //   <p>{price}</p>
+            //   <p>{description}</p>
+            //   <a href={photos}></a>
+            // </div>
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 }
