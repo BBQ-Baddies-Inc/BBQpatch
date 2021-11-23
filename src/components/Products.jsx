dc5e9ba4ceca7d8e88645cfd88c30bac9e6ba93c
Eric Flores <ericfullstack@gmail.com>
2021-11-22 20:03:19 -0800

    product.jsx fix

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,41 +20,42 @@ export default function Products() {
     <div>
       <h1>Products</h1>
       <div className="products_page">
-      <div className="card_positions">
-        {products.map((product) => {
-          const { name, price, description, photo} = product;
-          return (
-            <Card style={{ width: "18rem" }}>
-              <Card.Img src={photo}/>
-              <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Text>{description}</Card.Text>
-                <Card.Text>{price}</Card.Text>
-                {/* <Button variant="primary">Add to Cart</Button> */}
-                <form className="productButtons">
-                  <input
-                    type="number"
-                    // placeholder=“quantity-of-products”
-                    min="1"
-                    max="10"
-                  ></input>
-                  <Link to={`/cart`}>
-                    <button>Add To Cart</button>
-                  </Link>
-                </form>
-              </Card.Body>
-            </Card>
-            // <div className="card" key={product.id}>
-            //   <h2>{name}</h2>
-            //   <p>{price}</p>
-            //   <p>{description}</p>
-            //   <a href={photos}></a>
-            // </div>
-          );
-        })}
+        <div className="card_positions">
+          {products && products.length
+            ? products.map((product) => {
+                const { name, price, description, photo } = product;
+                return (
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img src={photo} />
+                    <Card.Body>
+                      <Card.Title>{name}</Card.Title>
+                      <Card.Text>{description}</Card.Text>
+                      <Card.Text>{price}</Card.Text>
+                      {/* <Button variant="primary">Add to Cart</Button> */}
+                      <form className="productButtons">
+                        <input
+                          type="number"
+                          // placeholder=“quantity-of-products”
+                          min="1"
+                          max="10"
+                        ></input>
+                        <Link to={`/cart`}>
+                          <button>Add To Cart</button>
+                        </Link>
+                      </form>
+                    </Card.Body>
+                  </Card>
+                  // <div className="card" key={product.id}>
+                  //   <h2>{name}</h2>
+                  //   <p>{price}</p>
+                  //   <p>{description}</p>
+                  //   <a href={photos}></a>
+                  // </div>
+                );
+              })
+            : null}
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 }
