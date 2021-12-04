34dede5d0c5807cb063addfe483e6d762a6a6994
Eric Flores <ericfullstack@gmail.com>
2021-12-04 11:26:36 -0800

    update BASE url for heroku

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-export const BASE = "http://localhost:5000/api/cart"
+export const BASE = "http://localhost:5000/api"
+// export const BASE = "https://fathomless-sea-11187.herokuapp.com/api"
 
 export async function getCart(){
-    const {data} = await axios.get(`${BASE}/`);
+    const {data} = await axios.get(`${BASE}/cart`);
     console.log(data)
 return data.cart;
 }
