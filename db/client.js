f3a12fc55289941e22a424feb282bdf6a6a2b243
Luke <reboluketion42@gmail.com>
2021-12-04 13:23:58 -0800

    changes to heroku db

--- db/client.js
+++ db/client.js
@@ -2,7 +2,13 @@
 const { Client } = require('pg');
 const DB_NAME = 'localhost:5432/bbqbaddies'
 const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
-const client = new Client(DB_URL);
+const client = new Client({
+  connectionString: DB_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 
 // database methods
 
